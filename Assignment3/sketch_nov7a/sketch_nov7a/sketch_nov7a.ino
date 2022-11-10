eb5b446c1962bb24ab49c725c5609fbb451a884c
JamesJellyfish <james.pham2@ontariotechu.net>
2022-11-10 18:32:06 -0500

    Unity

---
+++ Assignment3/sketch_nov7a/sketch_nov7a/sketch_nov7a.ino
@@ -0,0 +1,179 @@
+unsigned long lastTime = 0;
+
+void setup() {
+  Serial.begin(9600);
+  int X = 0;
+  pinMode(2, INPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
+  pinMode(10, OUTPUT);
+  pinMode(10, INPUT);
+  pinMode(11, OUTPUT);
+  pinMode(11, INPUT);
+  pinMode(3, INPUT);
+  pinMode(4, INPUT);
+  pinMode(5, INPUT);
+  pinMode(6, INPUT);
+  pinMode(7, INPUT);
+  pinMode(8, INPUT);
+  pinMode(9, INPUT);
+  pinMode(A1, INPUT);
+  pinMode(A2, INPUT);
+  pinMode(A3, INPUT);    
+}
+
+void loop() {
+    if (millis() > lastTime + 5000)
+    {
+      Serial.println("Still alive");
+      lastTime = millis();
+    }
+    
+    switch(Serial.read())
+    {
+      case 'A':
+        Serial.println("Recieved 'A' on Arduino!");
+        break;
+    }
+/*
+  if (analogRead(A4) > 1) 
+  {
+    Serial.println("2");
+  }
+*/
+  if (digitalRead(2) == HIGH) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    Serial.println("Mouse 1");
+    digitalWrite(LED_BUILTIN, LOW);
+    digitalWrite(10, HIGH);
+    if (digitalRead(10) == HIGH) {
+      Serial.println("Vibrating");
+    }
+    digitalWrite(10, LOW);
+    digitalWrite(11, HIGH);
+    if (digitalRead(11) == HIGH) {
+      Serial.println("Light On");
+    }
+    analogWrite(11, LOW);
+  }
+  if (digitalRead(3) == HIGH) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    Serial.println("Mouse 2");
+    digitalWrite(LED_BUILTIN, LOW);
+    digitalWrite(11, HIGH);
+    if (digitalRead(11) == HIGH) {
+      Serial.println("Light On");
+    }
+    analogWrite(11, LOW);
+    if (digitalRead(10) == HIGH) {
+      Serial.println("Vibrating");
+    }
+  }
+  if (digitalRead(4) == HIGH) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    Serial.println("Middle Mouse");
+    digitalWrite(LED_BUILTIN, LOW);
+    digitalWrite(11, HIGH);
+    if (digitalRead(11) == HIGH) {
+      Serial.println("Light On");
+    }
+    analogWrite(11, LOW);
+    if (digitalRead(10) == HIGH) {
+      Serial.println("Vibrating");
+    }
+  }
+  if (digitalRead(5) == HIGH) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    Serial.println("Side Button 1");
+    digitalWrite(LED_BUILTIN, LOW);
+    digitalWrite(10, HIGH);
+    if (digitalRead(10) == HIGH) {
+      Serial.println("Vibrating");
+    }
+    digitalWrite(11, HIGH);
+    if (digitalRead(11) == HIGH) {
+      Serial.println("Light On");
+    }
+    analogWrite(11, LOW);
+  }
+  if (digitalRead(6) == HIGH) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    Serial.println("Side Button 2");
+    digitalWrite(LED_BUILTIN, LOW);
+    digitalWrite(10, HIGH);
+    if (digitalRead(10) == HIGH) {
+      Serial.println("Vibrating");
+    }
+    digitalWrite(11, HIGH);
+    if (digitalRead(11) == HIGH) {
+      Serial.println("Light On");
+    }
+    analogWrite(11, LOW);
+  }
+  if (digitalRead(7) == HIGH) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    Serial.println("Joystick Input");
+    digitalWrite(LED_BUILTIN, LOW);
+    digitalWrite(10, HIGH);
+    if (digitalRead(10) == HIGH) {
+      Serial.println("Vibrating");
+    }
+    digitalWrite(11, HIGH);
+    if (digitalRead(11) == HIGH) {
+      Serial.println("Light On");
+    }
+    analogWrite(11, LOW);
+  }
+  if (digitalRead(8) == HIGH) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    Serial.println("Mouse 6");
+    digitalWrite(LED_BUILTIN, LOW);
+    digitalWrite(10, HIGH);
+    if (digitalRead(10) == HIGH) {
+      Serial.println("Vibrating");
+    }
+    digitalWrite(11, HIGH);
+    if (digitalRead(11) == HIGH) {
+      Serial.println("Light On");
+    }
+    analogWrite(11, LOW);
+  }
+  if (digitalRead(9) == HIGH) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    Serial.println("Mouse 7");
+    digitalWrite(LED_BUILTIN, LOW);
+    digitalWrite(10, HIGH);
+    if (digitalRead(10) == HIGH) {
+      Serial.println("Vibrating");
+    }
+    digitalWrite(11, HIGH);
+    if (digitalRead(11) == HIGH) {
+      Serial.println("Light On");
+    }
+    analogWrite(11, LOW);
+  }
+
+  if (analogRead(A1) != 511) {
+    Serial.print("Analog 1: ");
+    Serial.println(analogRead(A1));
+    Serial.println("");
+  }
+  if (analogRead(A2) != 511) {
+    Serial.print("Analog 2: ");
+    Serial.println(analogRead(A2));
+    Serial.println("");
+    delay(1000); // Wait for 1000 millisecond(s)
+  }
+
+  if (analogRead(A3) < 511) {
+    Serial.println("Scrolling Down");
+    Serial.println("");
+    delay(1000); // Wait for 1000 millisecond(s)
+  }
+  if (analogRead(A3) > 511) {
+    Serial.println("Scrolling Up");
+    Serial.println("");
+    delay(1000); // Wait for 1000 millisecond(s)
+  }
+
+}
---
+++ Unity Team C9/Assets/Leonardo.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Leonardo : MonoBehaviour
+{
+    public SerialController serialController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        serialController = GameObject.Find("SerialController").GetComponent<SerialController>();
+        Debug.Log("Press X or Z to execute some actions");
+
+    }
+
+    // Executed each frame
+    void Update()
+    {
+
+        //---------------------------------------------------------------------
+        // Send data
+        //---------------------------------------------------------------------
+
+        // If you press one of these keys send it to the serial device. A
+        // sample serial device that accepts this input is given in the README.
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            Debug.Log("Sending X");
+            serialController.SendSerialMessage("X");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            Debug.Log("Sending Z");
+            serialController.SendSerialMessage("Z");
+        }
+
+
+        //---------------------------------------------------------------------
+        // Receive data
+        //---------------------------------------------------------------------
+
+        string message = serialController.ReadSerialMessage();
+
+        if (message == null)
+            return;
+
+        // Check if the message is plain data or a connect/disconnect event.
+        if (ReferenceEquals(message, SerialController.SERIAL_DEVICE_CONNECTED))
+            Debug.Log("Connection established");
+        else if (ReferenceEquals(message, SerialController.SERIAL_DEVICE_DISCONNECTED))
+            Debug.Log("Connection attempt failed or disconnection detected");
+        else
+            Debug.Log("Message arrived: " + message);
+    }
+}
